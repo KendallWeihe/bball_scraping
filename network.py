24bfc46c557efb5e658f8edeca7d864d3c314675
Kendall Weihe <kendallweihe@gmail.com>
2016-12-21 15:50:35 -0500

    cleaning repo

--- network.py
+++ network.py
@@ -4,45 +4,37 @@ import numpy as np
 import pdb
 import glob
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
-'''
-To classify images using a recurrent neural network, we consider every image
-row as a sequence of pixels. Because MNIST image shape is 28*28px, we will then
-handle 28 sequences of 28 steps for every sample.
-'''
-
-# Parameters
 learning_rate = 0.001
 training_iters = 100000
-batch_size = 4
+batch_size = 50
 display_step = 5
 
-# Network Parameters
-n_input = 22 # MNIST data input (img shape: 28*28)
-# max_size = 400
-n_steps = 150 # timesteps
-n_hidden = 128 # hidden layer num of features
-n_classes = 1 # MNIST total classes (0-9 digits)
+n_input = 22
+n_steps = 175
+n_hidden = 500
+n_classes = 1
+n_predictions = 50
+n_regression_points = 0
 
-# prediction_data = np.genfromtxt("./ncaa_data/Louisville_Wichita_St.csv", delimiter=",")
-# n_steps = prediction_data.shape[0]
-# prediction_data = prediction_data[0:n_steps,:].reshape((1,n_steps,n_input))
-
-# tf Graph input
 x = tf.placeholder("float", [None, n_steps, n_input])
 y = tf.placeholder("float", [None, n_classes])
 
-# Define weights
 weights = {
-    'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+    'all_out' : tf.get_variable("weights_1", shape=[n_steps*n_hidden, n_classes],
+               initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32),
+    # 'out' : tf.get_variable("weights_1", shape=[n_hidden, n_classes],
+    #            initializer=tf.contrib.layers.xavier_initializer(), dtype=tf.float32),
+   # 'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
 }
+
 biases = {
-    'out': tf.Variable(tf.random_normal([n_classes]))
+    'out': tf.Variable(tf.zeros([n_classes]))
 }
 
 
 def input_data():
-    # files = glob.glob('./ncaa_data/*/*.csv')
     files = glob.glob("./ncaa_data/completed_games/*.csv")
     print "Number of files = " + str(len(files))
 
@@ -50,138 +42,134 @@ def input_data():
     ground_truth = []
     scores = []
     for csv_file in files:
-        csv_data = np.genfromtxt(csv_file, delimiter=",")
-        # halftime_index = csv_data.shape[0]
-        # for i in range(csv_data.shape[0]):
-        #     if csv_data[i,0] == 20 or csv_data[i,0] > 20:
-        #         halftime_index = i
-        #         break
-        # # pdb.set_trace()
-        # input_data.append(csv_data[0:halftime_index,:])
-        #TODO write a data verification script
-        if csv_data.shape[0] > n_steps:
-            input_data.append(csv_data[0:n_steps,:])
-            ground_truth.append(csv_data[csv_data.shape[0]-1,2] - csv_data[csv_data.shape[0]-1,3])
-            scores.append(csv_data[n_steps,2] - csv_data[n_steps,3])
+        try:
+            csv_data = np.genfromtxt(csv_file, delimiter=",")
+            if csv_data.shape[0] > n_steps:
+                input_data.append(csv_data[0:n_steps,:])
+                ground_truth.append(csv_data[csv_data.shape[0]-1,2] - csv_data[csv_data.shape[0]-1,3])
+                scores.append(csv_data[n_steps,2] - csv_data[n_steps,3])
+        except:
+            print csv_file
 
     return np.array(input_data), np.array(ground_truth), np.array(scores)
 
-# prediction_data = np.genfromtxt("./ncaa_data/Charleston_Villanova.csv", delimiter=",")
-# prediction_data = np.genfromtxt("./ncaa_data/Colgate_Penn_State.csv", delimiter=",")
-
-
 input_data, ground_truth, scores = input_data()
-training_data = input_data[0:58,:]
-training_ground_truth = ground_truth[0:58]
-prediction_data = input_data[58:66,:]
-prediction_ground_truth = ground_truth[58:66]
-prediction_scores = scores[58:66]
+randomize = np.arange(len(input_data))
+np.random.shuffle(randomize)
+input_data = input_data[randomize]
+ground_truth = ground_truth[randomize]
+
+training_data = input_data[0:input_data.shape[0] - n_predictions - n_regression_points, :]
+training_ground_truth = ground_truth[0:ground_truth.shape[0] - n_predictions - n_regression_points]
+prediction_data = input_data[input_data.shape[0] - n_predictions - n_regression_points:input_data.shape[0] - n_regression_points, :]
+prediction_ground_truth = ground_truth[ground_truth.shape[0] - n_predictions - n_regression_points:ground_truth.shape[0] - n_regression_points]
+regression_data = input_data[input_data.shape[0] - n_regression_points: input_data.shape[0], :]
+regression_ground_truth = ground_truth[ground_truth.shape[0] - n_regression_points: ground_truth.shape[0]]
 
 def RNN(x, weights, biases):
-
-    # Permuting batch_size and n_steps
     x = tf.transpose(x, [1, 0, 2])
-    # Reshaping to (n_steps*batch_size, n_input)
     x = tf.reshape(x, [-1, n_input])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
     x = tf.split(0, n_steps, x)
-
-    # Define a lstm cell with tensorflow
     lstm_cell = rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
-
-    # Get lstm cell output
     outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
-
-    # Linear activation, using rnn inner loop last output
-    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+    all_lstm_outputs = tf.reshape(tf.stack(outputs, axis=1), [-1, n_steps*n_hidden])
+    # output = tf.matmul(outputs[-1], weights['out']) + biases['out']
+    output = tf.matmul(all_lstm_outputs, weights['all_out']) + biases['out']
+    return tf.nn.dropout(output, 0.75)
 
 pred = RNN(x, weights, biases)
-
-# Define loss and optimizer
-# pdb.set_trace()
 n_samples = tf.cast(tf.shape(x)[0], tf.float32)
 cost = tf.reduce_sum(tf.pow(pred-y, 2))/(2*n_samples)
-# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-# Evaluate model
 accuracy = tf.abs(tf.sub(pred, y))
-
-# Initializing the variables
 init = tf.initialize_all_variables()
 saver = tf.train.Saver()
+
+# prediction_game = np.genfromtxt("./ncaa_data/Northeastern_Michigan_State.csv", delimiter=",")[0:n_steps,:]
 
 # Launch the graph
 with tf.Session() as sess:
     sess.run(init)
     step = 1
-    min_diff = 17
-    # Keep training until reach max iterations
-    avg_diff = []
-    while step * batch_size < training_iters:
-        acc_batch = []
-        loss_batch = []
-        # pdb.set_trace()
+    # step = 501
+    # saver.restore(sess, "./lstm_models/200/bball_scraping/lstm_models/200/lstm_model_n_steps_175_750_0.773770080762_1.33473893366_0.408350682807.ckpt")
+    avg_pred_diff = []
+    avg_pred_vals = []
+    avg_reg_vals = []
+    max_r_values = [0,0,0]
+    accuracy_data = []
+    single_game_pred = []
+    for step in range(10000):
+
         start_pos = np.random.randint(len(training_data) - batch_size)
-        # for i in range(start_pos,start_pos+4):
-        # for i in range(len(input_data)):
-        # pdb.set_trace()
         batch_x = training_data[start_pos:start_pos+batch_size].reshape((batch_size, n_steps, n_input))
-            # batch_x = input_data[i].reshape((1, input_data[i].shape[0], input_data[i].shape[1]))
         batch_y = training_ground_truth[start_pos:start_pos+batch_size].reshape((batch_size,n_classes))
-            # batch_y = ground_truth[i].reshape((1,1))
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-        # if step % display_step == 0:
-            # Calculate batch accuracy
-            # pdb.set_trace()
+
         acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-        acc_batch.append(acc[0][0])
-        # Calculate batch loss
+        accuracy_data.append(np.mean(acc))
         loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
-        loss_batch.append(loss)
-        # print "Step: " + str(step) + "  Accuracy: " + str(np.mean(np.array(acc_batch))) + "  Loss: " + str(np.mean(np.array(loss_batch)))
-        print "Step: " + str(step) + "  Accuracy: " + str(acc[0][0]) + "  Loss: " + str(loss)
-        step += 1
+        # print "Step: " + str(step) + "  Accuracy: " + str(acc[0][0]) + "  Loss: " + str(loss)
+        print "Step: " + str(step) + "  Mean Accuracy: " + str(np.mean(accuracy_data)) + "  Loss: " + str(loss)
 
-        pred_vals = []
-        local_avg = []
-        # pdb.set_trace()
-        for i in range(len(prediction_data)):
-            batch_x = prediction_data[i].reshape((1, prediction_data[i].shape[0], prediction_data[i].shape[1]))
-            batch_y = prediction_ground_truth[i]
-            pred_val = sess.run(pred, feed_dict={x: batch_x})
-            avg_diff.append(pred_val[0][0] - prediction_ground_truth[i])
-            local_avg.append(pred_val[0][0] - prediction_ground_truth[i])
-            pred_vals.append(pred_val[0][0])
-            print "Prediction = " + str(pred_val) + "  Actual = " + str(prediction_ground_truth[i]) + "  Score difference = " + str(prediction_scores[i])
+        if step > 250:
+            pred_vals = []
+            for i in range(prediction_data.shape[0]):
+                pred_val = sess.run(pred, feed_dict={x: prediction_data[i].reshape((1,n_steps,n_input))})
+                # pdb.set_trace()
+                pred_vals.append(int(pred_val))
+                avg_pred_diff.append(int(pred_val)-prediction_ground_truth[i])
+                # print "Prediction = " + str(pred_val[0][0]) + "  Actual = " + str(prediction_ground_truth[i])
+            # print "Prediction average = " + str(np.mean(np.abs(np.array(avg_pred_diff))))
 
-        # pred_val = sess.run(pred, feed_dict={x: prediction_data})
-        # print "Prediction = " + str(pred_val)
-        # avg_diff.append(pred_val)
-        print "Average = " + str(np.mean(np.abs(np.array(avg_diff))))
-        #print "Average difference = " + str(np.mean(np.abs(np.array(pred_val - prediction_ground_truth))))
-        print "Local average = " + str(np.mean(np.abs(np.array(local_avg))))
+            slope, intercept, r_value, p_value, std_err = linregress(np.array(pred_vals), prediction_ground_truth)
+        # print "Slope = " + str(slope)
+        # print "R^2 = " + str(r_value**2)
 
-        if step % 50 == 0:
-            prediction_ground_truth = prediction_ground_truth.tolist()
-            plot_data = []
-            for i in range(len(pred_vals)):
-                plot_data.append([prediction_ground_truth[i], pred_vals[i]])
-            # plot_data = [prediction_ground_truth, pred_vals]
-            # plt.plot(plot_data, 'ro')
-            plt.scatter(prediction_ground_truth, pred_vals)
-            plt.grid(True)
-            plt.axhline(0, color='black')
-            plt.axvline(0, color='black')
-            plt.axis((-40,40,-40,40))
-            plt.show()
-            # pdb.set_trace()
-            prediction_ground_truth = np.array(prediction_ground_truth)
+        # if step % 25 == 0:
 
-        #if np.mean(np.abs(np.array(pred_val - prediction_ground_truth))) < min_diff:
-        #    save_path = "./lstm_models/lstm_model_" + str(step) + ".ckpt"
-        #    saver.save(sess, save_path)
-        #    min_diff = np.mean(np.abs(np.array(pred_val - prediction_ground_truth)))
+        # if step > 750 and r_value**2 > 0.63 and r_value**2 > np.min(np.array(max_r_values)):
+        # if step > 750 and r_value**2 > 0.5:
+        if step > 250:
+            print "Step = " + str(step)
+            print "Accuracy = " + str(np.mean(acc))
 
+            avg_pred_vals.append(pred_vals)
+            avg_pred_vals_np = np.array(avg_pred_vals)
+            avg_vals = []
+            print "Raw prediction data ----------------------------------------------"
+            for i in range(avg_pred_vals_np.shape[1]):
+                avg_vals.append(np.mean(avg_pred_vals_np[:,i]))
+                print str(np.mean(avg_pred_vals_np[:,i])) + "," + str(prediction_ground_truth[i])
+                # print "Average for game " + str(i) + " = " + str(np.mean(avg_pred_vals_np[:,i])) + "  Actual = " + str(prediction_ground_truth[i])
+            print "Slope before average = " + str(slope)
+            print "R^2 before average = " + str(r_value**2)
+            slope, intercept, r_value, p_value, std_err = linregress(np.array(avg_vals), prediction_ground_truth)
+            print "Slope after average = " + str(slope)
+            print "R^2 after average = " + str(r_value**2)
+            print "Intercept after average = " + str(intercept)
+            max_r_values.append(r_value**2)
+            if len(max_r_values) > 8:
+                del max_r_values[np.where(np.array(max_r_values) == np.min(np.array(max_r_values)))[0][0]]
 
+            print "Actual prediction values --------------------------------------------"
+            for i in range(len(avg_vals)):
+                actual_value = slope * avg_vals[i] + intercept
+                print str(actual_value) + "," + str(prediction_ground_truth[i])
+
+            print "\n"
+
+            step += 1
+
+            # pred_val = sess.run(pred, feed_dict={x: prediction_game.reshape((1,n_steps,n_input))})
+            # print "Single game prediction = " + str(pred_val)
+            # single_game_pred.append(pred_val)
+            # print "Single game average prediction = " + str(np.mean(np.array(single_game_pred)))
+
+            # if step % 250 == 0:
+            #     save_path = "./lstm_models/200/lstm_model_n_steps_" + str(n_steps) + "_" + str(step) + "_" + str(r_value**2) + "_" + str(slope) + "_" + str(intercept) + ".ckpt"
+            #     saver.save(sess, save_path)
+            #     min_diff = np.mean(np.abs(np.array(pred_val - prediction_ground_truth)))
+    # plt.plot(accuracy_data)
+    # plt.show()
     print("Optimization Finished!")
